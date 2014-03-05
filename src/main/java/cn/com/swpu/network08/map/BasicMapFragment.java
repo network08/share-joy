93b7d5f79089c77851938f57eab11cdfa15597c9
franklinxkk <franklin.xkk@gmail.com>
2014-03-05 17:39:41 +0800

    add application ,fixed map load error, update map lib to the newest
    Signed-off-by: franklinxkk <franklin.xkk@gmail.com>

--- src/main/java/cn/com/swpu/network08/map/BasicMapFragment.java
+++ src/main/java/cn/com/swpu/network08/map/BasicMapFragment.java
@@ -1,6 +1,5 @@
 package cn.com.swpu.network08.map;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -26,15 +25,15 @@ public class BasicMapFragment extends Fragment implements OnClickListener{
 	/**
 	 *  MapView 是地图主控件
 	 */
-	private MapView mMapView = null;
+	private MapView basicMapView = null;
 	/**
 	 *  用MapController完成地图控制 
 	 */
-	private MapController mMapController = null;
+	private MapController basicMapController = null;
 	/**
 	 *  MKMapViewListener 用于处理地图事件回调
 	 */
-	MKMapViewListener mMapListener = null;
+	MKMapViewListener basicMapListener = null;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		MyApplication app = (MyApplication)getActivity().getApplication(); 
@@ -47,41 +46,14 @@ public class BasicMapFragment extends Fragment implements OnClickListener{
 			Bundle savedInstanceState) {
 		View messageLayout = inflater.inflate(R.layout.basic_map_layout,
 				container, false);
-		mMapView = (MapView)getActivity().findViewById(R.id.bmapView);
-		/**
-		 * 获取地图控制器
-		 */
-		mMapController = mMapView.getController();
-		/**
-		 *  设置地图是否响应点击事件  .
-		 */
-		mMapController.enableClick(true);
-		/**
-		 * 设置地图缩放级别
-		 */
-		mMapController.setZoom(12);
-
-		/**
-		 * 将地图移动至指定点
-		 * 使用百度经纬度坐标，可以通过http://api.map.baidu.com/lbsapi/getpoint/index.html查询地理坐标
-		 * 如果需要在百度地图上显示使用其他坐标系统的位置，请发邮件至mapapi@baidu.com申请坐标转换接口
-		 */
-		GeoPoint p ;
-		double cLat = 39.945 ;
-		double cLon = 116.404 ;
-		Intent  intent = getActivity().getIntent();
-		if ( intent.hasExtra("x") && intent.hasExtra("y") ){
-			//当用intent参数时，设置中心点为指定点
-			Bundle b = intent.getExtras();
-			p = new GeoPoint(b.getInt("y"), b.getInt("x"));
-		}else{
-			//设置中心点为天安门
-			p = new GeoPoint((int)(cLat * 1E6), (int)(cLon * 1E6));
-		}
-
-		mMapController.setCenter(p);
-
-		mMapListener = new MKMapViewListener() {
+		basicMapView = (MapView)messageLayout.findViewById(R.id.bmapView);
+		//获取地图控制器
+		basicMapController = basicMapView.getController();
+		basicMapController.enableClick(true);
+		//设置地图缩放级别
+		basicMapController.setZoom(12);
+		basicMapController.setCenter(new GeoPoint((int)(30.827* 1E6), (int)(104.189* 1E6)));//swpu
+		basicMapListener = new MKMapViewListener() {
 			@Override
 			public void onMapMoveFinish() {
 				/**
@@ -95,21 +67,21 @@ public class BasicMapFragment extends Fragment implements OnClickListener{
 				/**
 				 * 在此处理底图poi点击事件
 				 * 显示底图poi名称并移动至该点
-				 * 设置过： mMapController.enableClick(true); 时，此回调才能被触发
+				 * 设置过： basicMapController.enableClick(true); 时，此回调才能被触发
 				 * 
 				 */
 				String title = "";
 				if (mapPoiInfo != null){
 					title = mapPoiInfo.strText;
-					Toast.makeText(getActivity(),title,Toast.LENGTH_SHORT).show();
-					mMapController.animateTo(mapPoiInfo.geoPt);
+					showToast(title);
+					basicMapController.animateTo(mapPoiInfo.geoPt);
 				}
 			}
 
 			@Override
 			public void onGetCurrentMap(Bitmap b) {
 				/**
-				 *  当调用过 mMapView.getCurrentMap()后，此回调会被触发
+				 *  当调用过 basicMapView.getCurrentMap()后，此回调会被触发
 				 *  可在此保存截图至存储设备
 				 */
 			}
@@ -125,13 +97,10 @@ public class BasicMapFragment extends Fragment implements OnClickListener{
 			 */
 			@Override
 			public void onMapLoadFinish() {
-				Toast.makeText(getActivity(), 
-						"地图加载完成", 
-						Toast.LENGTH_SHORT).show();
-
+				showToast("地图加载完成");
 			}
 		};
-		mMapView.regMapViewListener(MyApplication.getInstance().getbMapManager(), mMapListener);
+		basicMapView.regMapViewListener(MyApplication.getInstance().getbMapManager(), basicMapListener);
 		return messageLayout;
 	}
 	@Override
@@ -141,19 +110,23 @@ public class BasicMapFragment extends Fragment implements OnClickListener{
 
 	@Override
 	public void onResume() {
-		mMapView.onResume();
+		basicMapView.onResume();
 		super.onResume();
 	}
 
 	@Override
 	public void onPause() {
-		mMapView.onPause();
+		basicMapView.onPause();
 		super.onPause();
 	}
 
 	@Override
 	public void onDestroy() {
-		mMapView.destroy();
+		basicMapView.destroy();
 		super.onDestroy();
+	}
+	
+	private void showToast(String content){
+		Toast.makeText(getActivity(), content, Toast.LENGTH_SHORT).show();
 	}
 }
