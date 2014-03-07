94be6d0a245af924ef6c8ab226dddc2a7292c5cb
franklinxkk <franklin.xkk@gmail.com>
2014-03-07 22:14:06 +0800

    增加首页图片浏览

--- src/main/java/cn/com/swpu/network08/fragment/HomeFragment.java
+++ src/main/java/cn/com/swpu/network08/fragment/HomeFragment.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.Toast;
 import cn.com.swpu.network08.R;
+import cn.com.swpu.network08.util.ImageViewAdapter;
+import cn.com.swpu.network08.util.Images;
 import cn.com.swpu.network08.util.LocationProvider;
 
 /**
@@ -15,13 +18,23 @@ import cn.com.swpu.network08.util.LocationProvider;
  *
  */
 public class HomeFragment extends Fragment implements OnClickListener{
+	private GridView imageGridView;
+	private ImageViewAdapter imageAdapter;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View messageLayout = inflater.inflate(R.layout.home_fragment_layout,
+		View messageLayout = inflater.inflate(R.layout.image_grid_layout,
 				container, false);
-		Toast.makeText(getActivity(), LocationProvider.getInstance(getActivity()).getLocation(), Toast.LENGTH_SHORT).show();
+		imageGridView = (GridView)messageLayout.findViewById(R.id.image_grid_view);
+		imageAdapter = new ImageViewAdapter(getActivity(), 0, Images.imageThumbUrls, imageGridView);
+		imageGridView.setAdapter(imageAdapter);
 		return messageLayout;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		imageAdapter.cancelAllTasks();
 	}
 	@Override
 	public void onClick(View v) {
